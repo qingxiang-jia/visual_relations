cfc21e176d98471f8e0720c31c1d6af7ad9bd7b8
pq <ybjqx3340@gmail.com>
2015-04-10 18:30:50 -0400

    Added extractSouth, fully tested

--- src/SpacialRelationExtractor.java
+++ src/SpacialRelationExtractor.java
@@ -55,17 +55,17 @@ public class SpacialRelationExtractor
         int numOfBuilding = area.length;
         for (int building = 0; building < numOfBuilding; building++) {
             extractNorth(building);
+            extractSouth(building);
         }
 
         // print result
         for (int i = 0; i < numOfBuilding; i++) {
             System.out.println("building:" + i);
-            for (int j = 0; j < north[i].length; j++) {
-                System.out.printf(" "+j+" ");
-                if (north[i][j])
+            for (int j = 0; j < south[i].length; j++) {
+                if (south[i][j]) {
+                    System.out.printf(" "+j+" ");
                     System.out.print("T\n");
-                else
-                    System.out.print("F\n");
+                }
             }
         }
     }
@@ -130,6 +130,66 @@ public class SpacialRelationExtractor
         }
     }
 
+    /**
+     * Extracts South(S, T) for each building.
+     * Algorithm:
+     * rMin, cMin----rMin, cMax
+     * |                      |
+     * |                      |
+     * |                      |
+     * |                      |
+     * |                      |
+     * |                      |
+     * rMax, cMin----rMax, cMax
+     *             |
+     *             |
+     *             |
+     *             V scan south
+     * Above is the MBR, initialize a scan whose width is roughly (cMax - cMin) to the
+     * north, any building touched in this scan will be T in South(S, T).
+     * @param building
+     */
+    private void extractSouth(int building)
+    {
+        int rowMin = rMin[building], rowMax = rMax[building];
+        int colMin = cMin[building], colMax = cMax[building];
+        int width = colMax - colMin + 1;
+
+        int scanBegin = colMin, scanEnd = colMax;
+        int rowBegin = rowMax + 10;
+        if (rowBegin > img.length - 1)
+            rowBegin = img.length - 1;
+
+        scanBegin -= 0.2 * width; // widen the scan width a little bit
+        if (scanBegin < 0)
+            scanBegin = 0;
+
+        scanEnd += 0.2 * width; // widen the scan width a little bit
+        if (scanEnd > img[0].length)
+            scanEnd = img[0].length - 1;
+
+        /** scan south **/
+        Set<Integer> southOf = new HashSet<>();
+        for (int r = rowBegin; r < img.length; r += 10)
+            for (int c = scanBegin; c <= scanEnd; c += 5) {
+                if (img[r][c] != 0) {
+                    /** check the slope formed by two centers of mass **/
+                    int x1 = centroids[building][1], y1 = centroids[building][0];
+                    int x2 = centroids[img[r][c] - 1][1], y2 = centroids[img[r][c] - 1][0];
+                    float m = getSlopeUpsideDown(x1, y1, x2, y2);
+                    if (!(Math.abs(m) < 1))
+                        southOf.add(img[r][c] - 1);
+                }
+            }
+
+        /** save result **/
+        for (int i = 0; i < north[building].length; i++) {
+            if (southOf.contains(i)) {
+                south[building][i] = true;
+            } // no else, all values initialized to false
+        }
+    }
+
     private float getSlopeUpsideDown(int x1, int y1, int x2, int y2)
     {
         if (x2 - x1 == 0) {
@@ -138,7 +198,7 @@ public class SpacialRelationExtractor
             else
                 return -Float.MAX_VALUE;
         }
-        return -((float) y2 - y1) / (x2 - x1);
+        return (-((float) y2 - y1)) / (x2 - x1);
     }
 
     public static void main(String[] args)
