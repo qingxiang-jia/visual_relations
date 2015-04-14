055f7aacad2d87a4c2b6eaf1a68c38898bf61e01
pq <ybjqx3340@gmail.com>
2015-04-14 14:13:57 -0400

    Added a POJO that stores building names.

---
+++ src/Id2Name.java
@@ -0,0 +1,41 @@
+/**
+ * POJO that simply translates numerical id of a building into String.
+ * id is the value of the building - 1.
+ */
+public class Id2Name
+{
+    final String[] buildingNames = new String[] {
+            "Pupin",
+            "Schapiro CEPSR",
+            "Mudd, Engineering Terrace, Fairchild & Computer Science",
+            "Physical Fitness Center",
+            "Gymnasium & Uris",
+            "Schermerhorn",
+            "Chandler & Havemeyer",
+            "Computer Center",
+            "Avery",
+            "Fayerweather",
+            "Mathematics",
+            "Low Library",
+            "St. Paul's Chapel",
+            "Earl Hall",
+            "Lewisohn",
+            "Philosophy",
+            "Buell & Maison Francaise",
+            "Alma Mater",
+            "Dodge",
+            "Kent",
+            "College Walk",
+            "Journalism & Furnald",
+            "Hamilton, Hartley, Wallach & John Jay",
+            "Lion's Court",
+            "Lerner Hall",
+            "Butler Library",
+            "Carman"
+    };
+
+    public String getName(int id)
+    {
+        return buildingNames[id];
+    }
+}
